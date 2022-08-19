8d71eca11d4190ba0b1694fe56a6b5a6f093e07c
plebea <bini060315kim@gmail.com>
2022-08-19 09:31:16 +0900

    add test api

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const getAPI = axios.create({
-    baseURL: process.env.VUE_API_URL,
-    withCredentials: true
+  baseURL: process.env.VUE_API_URL,
+  withCredentials: true
 })
 
+export async function connectionTest (): Promise<object> {
+  const test = await getAPI.get('/')
+  return test
+}
