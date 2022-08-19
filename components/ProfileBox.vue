691f60b057f8128c5d287e1cb443ef006fee8dec
plebea <bini060315kim@gmail.com>
2022-08-19 09:35:16 +0900

    create need vue files and set environment

---
+++ components/ProfileBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ProfileBox'
+})
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/Information/ProfileBox/style.scss';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,21 @@
 <template>
-  <Nuxt />
+  <div>
+    <div class="information">
+      <ProfileBox />
+      <LiveVideo />
+    </div>
+
+    <Nuxt />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import ProfileBox from '../components/ProfileBox.vue'
+import LiveVideo from '../components/LiveVideo.vue'
+
 export default Vue.extend({
-    
+  components: { ProfileBox, LiveVideo }
 })
 </script>
 
