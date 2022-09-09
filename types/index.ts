d7157886e97594504e8e084bca9a64b47766c92f
plebea <bini060315kim@gmail.com>
2022-09-10 02:26:44 +0900

    edit type

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,7 @@
 // 타입스크립트를 사용하기 위해 타입을 정의합니다.
 // 수난 사고가 일어났을 때 받아오는 데이터의 타입을 정의하였습니다.
 export interface AccidentList {
-    id: number;
+    id: string;
     time: Date;
     type: string;
     image: string;
@@ -17,6 +17,6 @@ export interface Accident {
 
 // 수난 사고 발생 시 사고 처리 수락 여부에 따라 반환하는 데이터의 타입을 정의하였습니다.
 export interface returnAccident {
-    id: number;
+    id: string;
     status: boolean;
 }
